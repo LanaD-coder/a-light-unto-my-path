c118dc5c098f14c2c466e3118ac5910c422f2b31
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-27 13:23:38 +0100

    fix deployment issues

--- my_project/settings.py
+++ my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)#o9+y)x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['alight-untomy-path-5986a905c2de.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
