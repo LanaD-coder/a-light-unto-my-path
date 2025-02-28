10a28a1feb8b18e2ce165b37eefad2e4440fa819
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-02-28 17:08:36 +0100

    Updated settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['alight-untomy-path-5986a905c2de.herokuapp.com', '127.0.0.1']
 
