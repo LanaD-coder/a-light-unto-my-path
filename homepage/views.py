4fe3253b81357a07d69405c205cad15f80f136f8
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-03-03 13:52:05 +0100

    add details to homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
-# Views for handling homepage-related requests.
-from django.http import HttpResponse
 
 
 # Create your views here.
 def homepage(request):
-    # Handles requests to the about page.
-    return HttpResponse('Welcome to my blog!')
-    # Returns an HTTP response with a message.
+    return render(request, 'homepage.html')  # Renders the homepage template
