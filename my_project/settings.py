43b28bff769a0da9d23d5448ca0b32fd2f251593
LanaD-cell <rrwycmqu@students.codeinstitute.net>
2025-03-19 10:56:43 +0100

    add READme details

--- my_project/settings.py
+++ my_project/settings.py
@@ -31,7 +31,7 @@ STATIC_DIR = BASE_DIR / 'static'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['alight-untomy-path-5986a905c2de.herokuapp.com', '127.0.0.1']
 
